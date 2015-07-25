d1a71dc232c4c2a97beca47eb1a1880e0f2c24bc
Priyav shah <priyavrocks@gmail.com>
2015-07-25 01:18:53 +0100

    Increased testing. Modified mongoose User schema to do more work

--- app/route_methods/user_methods.js
+++ app/route_methods/user_methods.js
@@ -1,36 +1,46 @@
-//var Ripple_Account = require('')
+var mongoose = require('mongoose'),
+    User = mongoose.model('User'),
+    Ripple_Account = mongoose.model('Ripple_Account');
+
 
 module.exports.save_user = function(req, res) {
     if (!req.body) return res.sendStatus(400);
+    // Check if correct ripple_details have been sent/ exist (?)
+    function save(account, wallet) {
+        User.save_user_and_wallet(account, wallet, function (err) {
+            if (err) return res.status(400).json(err);
 
-    var user = new User(req.body);
-
-    // Save User (if error, then return)
-    user.save(function (err) {
-        if (err) {
-            console.log(err);
-            res.sendStatus(400);
-            return res.end(err)
-        }
-
-        Ripple_Account.generate_wallet(function (err, wallet) {
-            if (err) return console.log("FUUCK");
-
-            // generate Ripple wallet
-            var wallet = new Ripple_Account(wallet);
-
-            // Update the User to also include pointer to ripple wallet
-            user.ripple_account = wallet._id;
-
-            wallet.save(function (err) {
-                if (err) console.error(err)
-            });
-
-            user.save(function (err) {
-                if (err) console.error(err)
-            })
+            res.json({success: true})
         });
+    }
 
-        res.send("It worked!")
-    });
-}
+    if(req.body.ripple_account && req.body.secret) {
+        var wallet = {
+            account: req.body.ripple_account,
+            secret: req.body.secret
+        };
+        save(req.body, wallet);
+    } else {
+        Ripple_Account.generate_wallet(function(err, wallet) {
+            save(req.body, wallet);
+        });
+    }
+};
+
+// Middle ware to make sure data is raw JSON
+module.exports.checkJSON = function(req, res, next) {
+    if (req.get('Content-Type') !== 'application/json') {
+        return res.status(400).json({
+            success: false,
+            error: {
+                message:"Content type must be application/json"
+            }
+        });
+    }
+    next();
+};
+
+//TODO validation check for ripple rest running
+
+
+
